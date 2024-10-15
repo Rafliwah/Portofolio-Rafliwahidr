0069a004263e8d930ef389b24af70fc9cadeddc6
Rafliwah <mrafliwahidr@gmail.com>
2024-10-15 13:02:48 +0700

    inisial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      space: {
+        '128': '12rem'
+      } 
+    },
+  },
+  plugins: [],
+}
+
